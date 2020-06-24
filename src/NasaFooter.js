9bf4fb5ed4a7f3650a297feff5f3eef812c63ae6
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-06-23 19:38:26 -0500

    MVP

--- src/NasaFooter.js
+++ src/NasaFooter.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {Button} from 'reactstrap';
+
 
 const NasaFooter = () => {
     return <div classname = 'footer'>
         <h6>Kyle Nelson 2020</h6>
-        <Button color="danger">This button does nothing!</Button>
+        
     </div>
 };
 
